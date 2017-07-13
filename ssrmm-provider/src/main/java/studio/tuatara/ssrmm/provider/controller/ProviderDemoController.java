c9dca53bf1c201535b42719a101ce2cb440e951b
Rjan <nox4@163.com>
2017-07-13 16:25:10 +0800

    config client refresh方式增加

--- ssrmm-provider/src/main/java/studio/tuatara/ssrmm/provider/controller/ProviderDemoController.java
+++ ssrmm-provider/src/main/java/studio/tuatara/ssrmm/provider/controller/ProviderDemoController.java
@@ -4,6 +4,7 @@
 package studio.tuatara.ssrmm.provider.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @RestController
+@RefreshScope
 public class ProviderDemoController {
 	
 	@Value("${demo.name}")
